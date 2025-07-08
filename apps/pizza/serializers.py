2feaa26c40157df6cc3a8d94de72d9d57c6592d8
Donato4Humans <donato.4work@gmail.com>
2025-07-08 23:56:12 +0300

    - finished hw2

--- apps/pizza/serializers.py
+++ apps/pizza/serializers.py
@@ -7,18 +7,3 @@ class PizzaSerializer(serializers.ModelSerializer):
     class Meta:
         model = PizzaModel
         fields = ('id', 'name', 'size', 'price', 'created_at', 'updated_at')
-    # id = serializers.IntegerField(read_only=True)
-    # name = serializers.CharField(max_length=50)
-    # size = serializers.IntegerField()
-    # price = serializers.FloatField()
-    # created_at = serializers.DateTimeField(read_only=True)
-    # updated_at = serializers.DateTimeField(read_only=True)
-    #
-    # def create(self, validated_data:dict):
-    #     return PizzaModel.objects.create(**validated_data)
-    #
-    # def update(self, instance, validated_data:dict):
-    #     for key, value in validated_data.items():
-    #         setattr(instance, key, value)
-    #     instance.save()
-    #     return instance

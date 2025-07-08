c30c5e74922409ce43c724571a6a165fd03be651
Donato4Humans <donato.4work@gmail.com>
2025-07-08 04:02:50 +0300

    - updated master

--- configs/urls.py
+++ configs/urls.py
@@ -15,8 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from first.views import FirstView
 
 urlpatterns = [
-    path('first', FirstView.as_view()),
+
 ]

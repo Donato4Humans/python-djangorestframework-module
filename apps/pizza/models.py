ea6131d694dbd3c9cb8a45c57f860ab47ba2c905
Donato4Humans <donato.4work@gmail.com>
2025-07-10 00:42:08 +0300

    - finished hw4

--- apps/pizza/models.py
+++ apps/pizza/models.py
@@ -20,14 +20,9 @@ class DaysChoices(models.TextChoices): # another way to validate further with ch
 class PizzaModel(BaseModel):
     class Meta:
         db_table = 'pizzas'
-        #ordering = ('id',) # if you need change default ordering in DB
 
-    # name = models.CharField(max_length=50, blank=True) # default model validation, blank allows to not fill name field
-    # size = models.IntegerField(default=22) # sets default value if not specified
-    # price = models.FloatField(null=True) # not proper way to use null, it is only advised to use with foreign keys when it`s deleted but you need save data
-    # pizza_shop = models.ForeignKey(PizzaShopModel, on_delete=models.CASCADE, related_name='pizzas')
-    name = models.CharField(max_length=20, validators=[V.RegexValidator(RegexEnum.NAME.pattern, RegexEnum.NAME.msg)]) # custom validator enum
-    size = models.IntegerField(validators=[V.MinValueValidator(1), V.MaxValueValidator(100)]) # ready val. from lib
+    name = models.CharField(max_length=20, validators=[V.RegexValidator(RegexEnum.NAME.pattern, RegexEnum.NAME.msg)])
+    size = models.IntegerField(validators=[V.MinValueValidator(1), V.MaxValueValidator(100)])
     price = models.FloatField()
-    day = models.CharField(max_length=9, choices=DaysChoices.choices) # as choices can be set only const entities like(days, months, etc.)
+    day = models.CharField(max_length=9, choices=DaysChoices.choices)
     pizza_shop = models.ForeignKey(PizzaShopModel, on_delete=models.CASCADE, related_name='pizzas')
